3c44159b9219ee9317c1c6fb42c00283a2466826
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-12-04 23:33:13 -0500

    Create project structure and calculator functions

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+//#region 
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return  x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+//#endregion
