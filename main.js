bb0463ccba411a9f56495e92246150a1a7cda017
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-12-04 23:52:49 -0500

    Implement operate function
    Created three different variables to hold an operator and two numbers.
    The operate function accepts the operator and both numbers and then uses
    a switch statement to determine what math function to run and returns
    the answer.

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+var num1 = 1;
+var num2 = 1;
+var operator = "";
 
+var test = operate(10,"/",2);
+console.log(test);
 
+function operate(num1,operator,num2) {
+    let answer = 1;
+    
+    switch(operator) {
+        case "+":
+            answer = add(num1,num2);
+            break;
+        case "-":
+            answer = subtract(num1,num2);
+            break;
+        case "*":
+            answer = multiply(num1,num2);
+            break;
+        case "/":
+            answer = divide(num1,num2);
+            break;
+    }
+
+    return answer;
+}
 
 //#region 
 function add(x,y) {
