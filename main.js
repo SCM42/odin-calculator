49900706235cb71513b57cdbca55ce95c041ba2e
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-12-08 20:11:39 -0500

    Populate calculator display when button clicked
    Added an eventlistener for clicking that stores the text content of the
    button in the num1 variable and then displays that value in the text
    content of the 'display' div container.

--- main.js
+++ main.js
@@ -1,45 +1,57 @@
-var num1 = 10;
-var num2 = 2;
-var operator = "+";
+let num1 = "";
+let num2 = "";
+let operator = "";
 
-var test = operate(num1,operator,num2);
-console.log(test);
+const buttons = document.querySelectorAll(".button");
+const displayText = document.getElementById("display");
 
-function operate(x,operator,y) {
-    let answer = 1;
-    
-    switch(operator) {
-        case "+":
-            answer = add(x,y);
-            break;
-        case "-":
-            answer = subtract(x,y);
-            break;
-        case "*":
-            answer = multiply(x,y);
-            break;
-        case "/":
-            answer = divide(x,y);
-            break;
+for (const button of buttons) {
+  button.addEventListener("click", function () {
+    if (button.id === "clear-sign") {
+      num1 = "";
+    } else {
+      num1 += button.textContent.trim();
     }
 
-    return answer;
+    displayText.textContent = num1;
+  });
 }
 
-//#region 
-function add(x,y) {
-    return x + y;
+//#region
+function operate(x, operator, y) {
+  let answer = 1;
+
+  switch (operator) {
+    case "+":
+      answer = add(x, y);
+      break;
+    case "-":
+      answer = subtract(x, y);
+      break;
+    case "*":
+      answer = multiply(x, y);
+      break;
+    case "/":
+      answer = divide(x, y);
+      break;
+  }
+
+  return answer;
 }
 
-function subtract(x,y) {
-    return  x - y;
+function add(x, y) {
+  return x + y;
 }
 
-function multiply(x,y) {
-    return x * y;
+function subtract(x, y) {
+  return x - y;
 }
 
-function divide(x,y) {
-    return x / y;
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
 }
 //#endregion
