8c66fccdebd832aa029020a733c5e708a89a8ebf
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-12-08 23:23:56 -0500

    Implement checkOperation function
    Calculator will now only perform operations on the first pair of numbers
    at a time if stringing together multiple operations.
    
    First I added a new array to hold only two values for num1 and the
    operator. If user inputs anything past the first operator, I use shift
    to assign values for num1 and operator from calcArr and then call
    getSecondNumber, which splits the display text into a temp array, finds
    the index for the last operator and then concatenates and returns the
    remaining elements into a single value to use for num2.
    
    Then I call operate and display the result. Now I just need to have
    selecting the equal sign call operate as well.

--- main.js
+++ main.js
@@ -2,21 +2,59 @@ let num1 = "";
 let num2 = "";
 let operator = "";
 
+let calcArr = [];
+
 const buttons = document.querySelectorAll(".button");
 const displayText = document.getElementById("display");
 
 for (const button of buttons) {
   button.addEventListener("click", function () {
-      updateDisplay(button);
+    checkForOperation(button);
+    updateDisplay(button);
   });
+}
+
+function checkForOperation(button) { 
+  if (button.classList.contains("operator")) {
+    //console.log(`Operator selected: "${button.textContent.trim()}"`);
+
+    if (calcArr.length === 2) {
+      num1 = parseInt(calcArr.shift());
+      operator = calcArr.shift();
+      num2 = parseInt(getSecondNumber());
+
+      let answer = operate(num1, operator, num2);
+      displayText.textContent = answer;
+    }
+    
+    calcArr.push(displayText.textContent.trim());
+    calcArr.push(button.textContent.trim());
+
+    //console.log(calcArr);
+  }
+}
+
+function getSecondNumber() {
+  let displayArr = displayText.textContent.trim().split("");
+  let operatorIndex = displayArr.findIndex((char) => char === operator);
+  let secondNumber = "";
+
+  //console.log(`Operator index is: ${operatorIndex}`);
+  //console.log(`Temporary array is ${displayArr}`);
+
+  for (i = operatorIndex + 1; i < displayArr.length; i++) {
+    secondNumber += displayArr[i];
+  }
+
+  return secondNumber;
 }
 
 function updateDisplay(button) {
   if (button.id === "clear-sign") {
-      displayText.textContent = "";
-    } else {
-      displayText.textContent += button.textContent.trim();
-    }
+    displayText.textContent = "";
+  } else {
+    displayText.textContent += button.textContent.trim();
+  }
 }
 
 //#region
